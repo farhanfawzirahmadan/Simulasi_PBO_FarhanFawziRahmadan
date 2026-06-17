321107d8725fe00da5c2460aaaf73f1c846eb6ae
unknown <farhanfawzirahmadan@gmail.com>
2026-06-17 10:10:28 +0700

    [Tahap 3]: Membuat koneksi dan abstract class

---
+++ koneksi.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db_name = "DB_SIMULASI_PBO_TI1C_FarhanFawziRahmadan";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch(PDOException $exception) {
+            echo "Koneksi database gagal: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
